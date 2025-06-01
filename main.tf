eb629f11f132ee98f88596cbd44b1e40269f083a
BuildTools <unconfigured@null.spigotmc.org>
2025-06-01 17:43:40 +0200

    add terraform configuration

---
+++ main.tf
@@ -0,0 +1,69 @@
+resource "kubernetes_namespace" "hello" {
+    metadata {
+        name = "hello"
+    }
+}
+
+resource "kubernetes_manifest" "deployment_hello_deployment" {
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "labels" = {
+        "app" = "hello"
+      }
+      "name" = "hello-deployment"
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "app" = "hello"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = "hello"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "image" = "kaldyka/hello-go:latest"
+              "name" = "hello-container"
+              "ports" = [
+                {
+                  "containerPort" = 80
+                },
+              ]
+            },
+          ]
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "service_hello_service" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Service"
+    "metadata" = {
+      "name" = "hello-service"
+    }
+    "spec" = {
+      "ports" = [
+        {
+          "port" = 80
+          "protocol" = "TCP"
+          "targetPort" = 80
+        },
+      ]
+      "selector" = {
+        "app" = "hello"
+      }
+      "type" = "LoadBalancer"
+    }
+  }
+}
